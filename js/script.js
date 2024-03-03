11a3161dc32606193736616f583a8379a557eea7
hasanali207 <hasanali142228@gmail.com>
2024-03-04 01:18:03 +0600

    added latest post section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const loadAllPost = async (searchValue="music" ) =>{
+const loadAllPost = async (searchValue="comedy" ) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchValue}`);
     const data = await res.json();
     const post = data.posts;
@@ -24,9 +24,13 @@ const loadAllPost = async (searchValue="music" ) =>{
         div.classList = 'flex flex-col lg:flex-row space-x-4  bg-[#F3F3F5] rounded-xl p-10 mb-6'
         div.innerHTML = `
         <div id="profile-thumb" class="profile-img">
-        <img class="" src="${item.image}" alt="" />
+          <div class="relative">
+            <div class="w-4 h-4 rounded-full absolute top-0 right-0 ${post?.isActive ? "bg-#10B981]" : "bg-[#FF3434]"} "> </div>
+              <img class="" src="${item.image}" alt="" />
+          </div>
+        
       </div>
-      <div class="">
+      <div class="w-full">
         <div class="flex space-x-5 items-center">
           <h3 class="text-[#12132D80]"># ${item.category}</h3>
           <h2 class="text-[#12132D80]">Author: ${item.author.name} </h2>
@@ -55,8 +59,8 @@ const loadAllPost = async (searchValue="music" ) =>{
         postContainer.appendChild(div)
         
     });
-    loadingSpinner(false)
-
+    
+    setTimeout(loadingSpinner(false), 2000)
     
 
 
@@ -96,9 +100,6 @@ const readMe = (title, view_count) =>{
 
 
 
-
-
-
 const handleSearch = () =>{
   loadingSpinner(true);
  const searchText   = document.getElementById('input-field').value;
@@ -116,6 +117,46 @@ const loadingSpinner = (isLoading) =>{
     loader.classList.add('hidden')
   }
 }
+
+
+
+// latest post conatainer  
+const latestPost = async () =>{
+  const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+    const post = await response.json();
+    const latestPostContainer = document.getElementById('latest-post-container');
+    
+    post.forEach((item) => {
+      console.log(item);
+      
+      const div = document.createElement('div');
+      div.classList = 'card  bg-base-100 border border-gray-300';
+      div.innerHTML = `
+      <figure class="px-6 pt-6">
+              <img src="${item.cover_image}" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body">
+              <p class="text-[#12132D80]"><i class="fa-solid fa-calendar-check"></i> ${item.author.posted_date}</p>
+              <h2 class="card-title text-[#12132D] font-extrabold">${item.title}</h2>
+              <p class="text-[#12132D] font-normal">${item.description}</p>
+              <div class="author-details flex items-center space-x-6 mt-2">
+                <img class="w-14 rounded-full" src="${item.profile_image}" alt="">
+                <div>
+                  <h1 class="text-[#12132D] font-extrabold">${item.author.name}</h1>
+                  <p class="text-[#12132D80]">${item.author.designation}</p>
+                </div>
+              </div>
+            </div>
+      `
+      latestPostContainer.appendChild(div)
+
+    });
+  
+    
+}
+
+latestPost();
+
 
 
 
