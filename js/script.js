2a85c12b0a23639df7a6836948c48ea285f170e8
hasanali207 <hasanali142228@gmail.com>
2024-03-04 00:02:42 +0600

    added view container append

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-const loadAllPost = async (catId) =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${item.category}`);
+const loadAllPost = async (searchValue="music" ) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchValue}`);
     const data = await res.json();
     const post = data.posts;
   
-
-    
     const postContainer = document.getElementById('post-container');
+    // let profileThumb = document.getElementById('profile-thumb');
+    postContainer.innerHTML = ''
+ 
+    post.forEach((item) => {
     
 
-    post.forEach((item) => {
+        // if (item?.isActive) {
+          
+        //   profileThumb.classList.add("online")
+        // } else {
+          
+        //   profileThumb.classList.add("offline")
+        // }
+
+        
+
         const div = document.createElement('div')
         div.classList = 'flex flex-col lg:flex-row space-x-4  bg-[#F3F3F5] rounded-xl p-10 mb-6'
         div.innerHTML = `
-        <div id="profile-thumb" class="profile-img offline">
+        <div id="profile-thumb" class="profile-img">
         <img class="" src="${item.image}" alt="" />
       </div>
       <div class="">
@@ -35,32 +46,81 @@ const loadAllPost = async (catId) =>{
             <li><i class="fa-regular fa-clock"></i> ${item.posted_time}min</li>
           </ul>
           <div>
-            <button onclick=readMe(); class="btn btn-active btn-accent text-white rounded-full"><i class="fa-regular fa-envelope-open"></i></button>
+            <button onclick="readMe('${item.title}', '${item.view_count}')" class="btn btn-active btn-accent text-white rounded-full"><i class="fa-regular fa-envelope-open"></i></button>
           </div>
         </div>
       </div>
         `
-        postContainer.appendChild(div);
-
         
-    })
+        postContainer.appendChild(div)
+        
+    });
+    loadingSpinner(false)
 
+    
 
 
 }
-loadAllPost();
-
-
 
 let count = 0;
-
+const viewContainer = document.getElementById('view-container')
 const readCount = document.getElementById("view-count");
-const readMe = () =>{
-    
-    count++;
-    readCount.innerText = count;
+const readMe = (title, view_count) =>{
+  
+  const div = document.createElement('div')
+  div.classList = 'bg-white flex p-4 justify-between items-center mt-4'
+  div.innerHTML = `
+  <h1 class="font-black my-4">
+                   ${title}
+                        </h1>
+                        <div>
+                          <div class="flex items-center justify-center">
+                            <i class="fa-regular fa-eye"></i>  
+                            <p class="ml-2">${view_count}</p>
+                          </div>
+                        </div>
+  
+  `;
 
+
+  viewContainer.appendChild(div)
+  
+  
+  count++;
+  readCount.innerText = count;
+
+
+ 
+ 
 }
+
+
+
+
+
+
+const handleSearch = () =>{
+  loadingSpinner(true);
+ const searchText   = document.getElementById('input-field').value;
+ 
+ loadAllPost(searchText);
+  
+  
+}
+loadAllPost();
+const loadingSpinner = (isLoading) =>{
+  const loader = document.getElementById('loading');
+  if(isLoading){
+    loader.classList.remove('hidden')
+  }else{
+    loader.classList.add('hidden')
+  }
+}
+
+
+
+
+
 
 
 
