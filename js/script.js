39c73b525912af72c62ad2913df3f28507e90946
hasanali207 <hasanali142228@gmail.com>
2024-03-04 12:28:51 +0600

    added default all post content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,59 @@
-const loadAllPost = async (searchValue="comedy" ) =>{
+const allPostDefault =  async () =>{
+  const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
+  const data = await res.json();
+  const post = data.posts;
+    
+    const postContainer = document.getElementById('post-container');
+    // let profileThumb = document.getElementById('profile-thumb');
+    postContainer.innerHTML = ''
+    post.forEach((item) => {
+    
+      const div = document.createElement('div')
+      div.classList = 'flex flex-col lg:flex-row space-x-4 bg-[#F3F3F5] hover:bg-[#797DFC20] rounded-xl p-10 mb-6'
+      div.innerHTML = `
+      <div id="profile-thumb" class="profile-img">
+        <div class="relative">
+          <div class="w-4 h-4 rounded-full absolute top-0 right-0 ${item?.isActive ? "bg-[#10B981]" : "bg-[#FF3434]"} "> </div>
+            <img class="" src="${item.image}" alt="" />
+        </div>
+      
+    </div>
+    <div class="w-full">
+      <div class="flex space-x-5 items-center">
+        <h3 class="text-[#12132D80]"># ${item.category}</h3>
+        <h2 class="text-[#12132D80]">Author: ${item.author.name} </h2>
+      </div>
+      <div>
+        <h1 class="text-xl font-black my-4">
+          ${item.title}
+        </h1>
+        <p class="text-[#12132D60] font-normal">
+          ${item.description}
+        </p>
+      </div>
+      <div class="flex items-center justify-between mt-6">
+        <ul class="flex space-x-6  items-center">
+          <li><i class="fa-regular fa-message"></i> ${item.comment_count}</li>
+          <li><i class="fa-regular fa-eye"></i> ${item.view_count}</li>
+          <li><i class="fa-regular fa-clock"></i> ${item.posted_time}min</li>
+        </ul>
+        <div>
+          <button onclick="readMe('${item.title}', '${item.view_count}')" class="btn btn-active btn-accent text-white rounded-full"><i class="fa-regular fa-envelope-open"></i></button>
+        </div>
+      </div>
+    </div>
+      `
+      
+      postContainer.appendChild(div)
+      
+  });
+      
+
+}
+
+allPostDefault();
+
+const loadAllPost = async (searchValue ) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchValue}`);
     const data = await res.json();
     const post = data.posts;
@@ -9,17 +64,6 @@ const loadAllPost = async (searchValue="comedy" ) =>{
  
     post.forEach((item) => {
     
-
-        // if (item?.isActive) {
-          
-        //   profileThumb.classList.add("online")
-        // } else {
-          
-        //   profileThumb.classList.add("offline")
-        // }
-
-        
-
         const div = document.createElement('div')
         div.classList = 'flex flex-col lg:flex-row space-x-4 bg-[#F3F3F5] hover:bg-[#797DFC20] rounded-xl p-10 mb-6'
         div.innerHTML = `
@@ -65,6 +109,8 @@ const loadAllPost = async (searchValue="comedy" ) =>{
 
 
 }
+
+
 
 let count = 0;
 const viewContainer = document.getElementById('view-container')
